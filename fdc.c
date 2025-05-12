9ca871a6d9117509a17094bf9db2974264c8ff84
Raji Sahansra <ssahansra@student.sierracollege.edu>
2025-05-12 16:47:15 -0700

    added quit functionality

--- fdc.c
+++ fdc.c
@@ -10,6 +10,8 @@
 int main(int argc, char *argv[]){
     //call vars and set arrays
     char output[100];
+    char input[100];
+    int quit = 0;
 
     //connect to server
     printf("connecting to: richmond.cs.sierracollege.edu\n");
@@ -21,10 +23,23 @@ int main(int argc, char *argv[]){
         printf("Can't make connection\n");
         exit(1);
     }
+    while(!quit){
+        FILE *s = fdopen(fd, "r+");
+        fprintf(s,"HELO");
+        fgets(output, 100, s);
+        printf("%s", output);
+        //prompt user for input
+        printf("What would you like to do?:");
+        fgets(input,100,stdin);
+        fprintf(s, "%s",input);
     
-    FILE *s = fdopen(fd, "r+");
-    fprintf(s,"HELO");
-    fgets(output, 100, s);
-    printf("%s", output);
-    
+        //chceck if user wants to end
+        if (strcmp(input, "QUIT\n") == 0) {
+            fprintf(s,"%s",input);
+
+            fclose(s);
+            close(fd);
+            quit = 1;
+        }
+    }
 }
