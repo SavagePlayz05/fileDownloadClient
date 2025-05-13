0a8cbce40234697ffe9f33a2871ee89b2ed0542d
Raji Sahansra <ssahansra@student.sierracollege.edu>
2025-05-12 17:41:14 -0700

    opened temp file using GET

--- fdc.c
+++ fdc.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]){
     //call vars and set arrays
     char output[100];
     char input[100];
+    char fileName[100];
     int quit = 0;
 
     //connect to server
@@ -23,16 +24,15 @@ int main(int argc, char *argv[]){
         printf("Can't make connection\n");
         exit(1);
     }
+    FILE *s = fdopen(fd, "r+");
     while(!quit){
-        FILE *s = fdopen(fd, "r+");
-        fprintf(s,"HELO");
-        fgets(output, 100, s);
-        printf("%s", output);
         //prompt user for input
         printf("What would you like to do?:");
         fgets(input,100,stdin);
+        printf("%s", input);
         fprintf(s, "%s",input);
-    
+        fflush(s);
+
         //chceck if user wants to end
         if (strcmp(input, "QUIT\n") == 0) {
             fprintf(s,"%s",input);
@@ -40,6 +40,24 @@ int main(int argc, char *argv[]){
             fclose(s);
             close(fd);
             quit = 1;
+        }
+
+        if (strcmp(input, "LIST\n") ==0) {
+            //while there is still lines left, and the last line doesn't have . as element 0.
+            while(fgets(output, 100, s) && (output[0] !='.')){
+                printf("%s", output);
+            }
+        }
+        if (strncmp(input, "GET ", 4) == 0) {
+            // remove +OK line
+            for (int i =0; i< (strlen(input) - 4); i++){
+                fileName[i] = input[i + 4];
+            }
+            printf("%s", fileName);
+            fgets(output, 100, s);
+            FILE *file = fopen(fileName, "w");
+
+
         }
     }
 }
